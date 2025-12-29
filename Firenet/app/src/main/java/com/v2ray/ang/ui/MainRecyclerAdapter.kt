28e573ec0137006c8abc9d56d2a2eb029d575555
Hossein <91972306+Hossein383@users.noreply.github.com>
2025-12-29 12:51:59 +0330

    Update MainRecyclerAdapter.kt

--- Firenet/app/src/main/java/com/v2ray/ang/ui/MainRecyclerAdapter.kt
+++ Firenet/app/src/main/java/com/v2ray/ang/ui/MainRecyclerAdapter.kt
@@ -15,7 +15,6 @@ import androidx.core.content.ContextCompat
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.RecyclerView
 import com.v2ray.ang.AppConfig
-import com.blongho.country_data.World
 import com.v2ray.ang.R
 import com.v2ray.ang.databinding.ItemRecyclerMainBinding
 import com.v2ray.ang.handler.MmkvManager
